f5f95cd6d73f7c44c9c6aa49e47f2eeba9544bfe
JuliaMoskvina <juliym13@yandex.ru>
2022-05-18 13:00:03 +0300

    Navigation Drawer Activity became main class.

--- app/src/main/java/ru/myitschool/vsu/g5/moskvina_y_m/archimedia/SplashScreen.java
+++ app/src/main/java/ru/myitschool/vsu/g5/moskvina_y_m/archimedia/SplashScreen.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import ru.myitschool.vsu.g5.moskvina_y_m.archimedia.MainActivity;
-
 public class SplashScreen extends AppCompatActivity {
 
     @Override
