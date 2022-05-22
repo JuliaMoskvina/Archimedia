1c43d31e2c3744276c2e3b9cbb95ab8a62e963a6
JuliaMoskvina <juliym13@yandex.ru>
2022-05-22 13:34:28 +0300

    I deleted ViewModel classes and also I created model classes for JSON file.

---
+++ app/src/main/java/ru/myitschool/vsu/g5/moskvina_y_m/archimedia/MaterialsActivity.java
@@ -0,0 +1,14 @@
+package ru.myitschool.vsu.g5.moskvina_y_m.archimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaterialsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materials);
+    }
+}
--- app/src/main/java/ru/myitschool/vsu/g5/moskvina_y_m/archimedia/ui/home/HomeFragment.java
+++ app/src/main/java/ru/myitschool/vsu/g5/moskvina_y_m/archimedia/ui/home/HomeFragment.java
@@ -17,26 +17,20 @@ import ru.myitschool.vsu.g5.moskvina_y_m.archimedia.databinding.FragmentHomeBind
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
